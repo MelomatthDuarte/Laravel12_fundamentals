32590cffd26af1964bf5ef4218e7dc1eca8a844d
melomatthewdev <devscustomers@outlook.com>
2025-05-09 22:11:34 -0500

    ruta posts

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route::get     | Consultar
- * Route::post    | Guardar
- * Route::delete  | Eliminar
- * Route::put     | Actualizar
- */
-
-/**
- * Route::get('/', function () {
- * return 'Ruta home';
- * });
- * 
- * Route::get('blog', function () {
- * return 'Listado de blogs';
- * });
- * 
- * Route::get('blog/{slug}', function ($slug) {
- * // Consulta a base de datos
- * return $slug;
- * });
- * 
- * Route::get('buscar', function (Request $request) {
- * return $request->all();
- * });
- */
-
-
- Route::controller(PageController::class)->group(function(){
-    Route::get('/', 'home')->name('home');
-    Route::get('blog', 'blog')->name('blog');
-    Route::get('blog/{post:slug}', 'post')->name('post');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('posts', PostController::class)->except('show');
+
 require __DIR__.'/auth.php';
+// require __DIR__.'/profile.php'; ← comenta o elimina esta línea
