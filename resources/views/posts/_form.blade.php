7f755a9aff1d67343c979f9305862ba33cf4f5e9
melomatthewdev <devscustomers@outlook.com>
2025-05-10 22:01:44 -0500

    validacion de resgistros unicos

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -8,6 +8,14 @@ Formulario
 @enderror
 <input type="text" name="title" class="rounded border-gray-200 w-full mb-4" value="{{ old('title', $post->title) }}">
 
+
+<label for="slug" class="uppercase text-gray-700 text-xs">Slug</label>
+@error('title')
+    <span class="text-xs text-red-500">{{ $message }}</span>
+@enderror
+<input type="text" name="slug" class="rounded border-gray-200 w-full mb-4" value="{{ old('slug', $post->slug) }}">
+
+
 <label for="body" class="uppercase text-gray-700 text-xs">Contenido</label>
 @error('body')
     <span class="text-xs text-red-500">{{ $message }}</span>
